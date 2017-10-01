9f035ed08841ce5ef697699393260d374bc99926
sanyokbig <sanyokbig@gmail.com>
2017-10-01 16:55:06 +0300

    статик дата добавлена

---
+++ src/staticData.js
@@ -0,0 +1,45 @@
+export const instaUsers = [
+    {
+        name: 'Insta user 1',
+        followers: 12,
+        postPrice: 100
+    },
+    {
+        name: 'Insta user 2',
+        followers: 54,
+        postPrice: 200
+    },
+    {
+        name: 'Insta user 3',
+        followers: 876,
+        postPrice: 600
+    },
+    {
+        name: 'Insta user 4',
+        followers: 12,
+        postPrice: 100
+    },
+    {
+        name: 'Insta user 5',
+        followers: 54,
+        postPrice: 200
+    }
+];
+
+export const youTubeUsers = [
+    {
+        name: 'Youtube user 1',
+        followers: 45,
+        postPrice: 345
+    },
+    {
+        name: 'Youtube user 2',
+        followers: 75,
+        postPrice: 536
+    },
+    {
+        name: 'Youtube user 3',
+        followers: 53,
+        postPrice: 244
+    }
+];
