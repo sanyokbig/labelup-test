e9fa7f487d6a4b7d1b7dc4d45c54767662e3712b
sanyokbig <sanyokbig@gmail.com>
2017-10-03 11:00:23 +0300

    враппер аккаунтов и индекс файл

---
+++ src/app/components/Accounts/Accounts.jsx
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {connect} from "react-redux";
+
+import List from './List.jsx';
+import ShowMore from './ShowMore.jsx';
+import Toggle from './Toggle.jsx'
+
+class Accounts extends Component {
+    render(){
+        const {accounts, pagination, list} = this.props;
+
+        return <div>
+            <Toggle currentList={list}/>
+            <List accounts={accounts}/>
+            <ShowMore onNextPage={pagination.onNextPage}/>
+        </div>
+    }
+}
+
+
+function mapStateToProps(state) {
+    const {accounts, pagination, list} = state.accountsList;
+
+    return {accounts, pagination, list}
+}
+
+
+
+export default connect(mapStateToProps)(Accounts);
