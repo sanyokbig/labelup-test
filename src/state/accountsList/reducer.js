28dc6864626031bc2a0b37b6e60705b6d79ee228
sanyokbig <sanyokbig@gmail.com>
2017-10-03 10:53:11 +0300

    редьюсер для переключений

---
+++ src/state/accountsList/reducer.js
@@ -0,0 +1,29 @@
+import staticData from '../../staticData';
+import {TOGGLE} from './actions';
+
+const initialState = {
+    accounts: staticData.instagram.slice(0, 3),
+    pagination: {
+        nextPage: 1,
+        onNextPage: 2,
+        totalCount: 5
+    },
+    list: 'instagram'
+};
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case TOGGLE:
+            return Object.assign({}, state, {
+                accounts: action.data.accounts,
+                pagination: {
+                    nextPage: 1,
+                    onNextPage: action.data.onNextPage,
+                    totalCount: action.data.totalCount
+                },
+                list: action.data.list
+            });
+        default:
+            return state;
+    }
+}
